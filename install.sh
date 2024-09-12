5c304aa9f57b60ee06c074e0e68b0b7d12e9eaab
Juan J. Garcia <juan.garcia@spinncloud.com>
2024-09-11 22:35:55 -0500

    Fix installation commands

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 sudo apt update && sudo apt full-upgrade -y
 
 # Install essential packages
-sudo apt install -y build-essential \\
-    neovim nano bat gedit curl git wget gpg net-tools unzip \\
-    neofetch \\
-    python3 python3-venv 
+sudo apt install -y build-essential \
+    neovim nano bat gedit curl git wget gpg net-tools unzip \
+    neofetch \
+    python3 python3-venv
 
 mkdir -p ~/.local/bin && ln -s /usr/bin/batcat ~/.local/bin/bat
 
@@ -26,18 +26,17 @@ for key in "${keys[@]}"; do
     ssh-keygen -N "" -f ~/.ssh/id_rsa_$key -C "$(whoami)@$(hostname)_for_$key"
 done
 # ssh-keygen -p -f ~/.ssh/id_rsa        For changing passphrase
-cp ~/.install/files/ssh-config ~/.ssh/
+cp ~/.install/files/ssh-config ~/.ssh/config
 
 # Change shell from bash to zsh with oh-my-zsh
-sudo apt install zsh
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
 mv ~/.zshrc ~/.zshrc-orig
 (echo "# User specific definitions\nif [ -f ~/.rc.d/init.rc ]; then\n    source ~/.rc.d/init.rc\nfi\n\n"; cat ~/.zshrc-orig) >  ~/.zshrc
 apt_pref='apt' && apt_upgr='upgrade'
 sed -i 's/plugins=(git)/plugins=(git z zsh-syntax-highlighting python debian)/' ~/.zshrc
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp ~/.install/files/.p10k.zsh ~/
 
