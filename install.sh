4d18cd422b32b01da23d2a0125a1966f5738bb78
Juan J. Garcia <juan.garcia@spinncloud.com>
2024-09-11 23:28:22 -0500

    Fix installation commands

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ cp ~/.install/files/ssh-config ~/.ssh/config
 
 # Change shell from bash to zsh with oh-my-zsh
 sudo apt install -y zsh
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'Y' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 mv ~/.zshrc ~/.zshrc-orig
 (cat .install/files/zsh_start; cat ~/.zshrc-orig) >  ~/.zshrc
