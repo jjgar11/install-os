768ab557383f8f18a5109422f0bee74f01e4514d
Juan J. Garcia <juan.garcia@spinncloud.com>
2025-01-14 05:49:12 -0500

    remove tabs

--- files/scripts/essentials.sh
+++ files/scripts/essentials.sh
@@ -5,12 +5,12 @@ sudo apt update && sudo apt full-upgrade -y
 
 # Install essential packages
 sudo apt install -y \
-    build-essential \
-    nano gedit curl git wget gpg net-tools unzip \
-    neofetch \
-    python3
+build-essential \
+nano gedit curl git wget gpg net-tools unzip \
+neofetch \
+python3
 sudo apt install -y \
-    make libssl-dev zlib1g-dev \
-    nlibbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev \
-    nlibncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
-    python3-openssl
+make libssl-dev zlib1g-dev \
+nlibbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev \
+nlibncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
+python3-openssl
