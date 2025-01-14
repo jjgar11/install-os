dc015eab33a6cb89d482e3bc79e3a1f8831aedaf
Juan J. Garcia <juan.garcia@spinncloud.com>
2025-01-14 03:58:40 -0500

    Fix pyenv error

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ source ~/.install/files/scripts/essentials.sh
 sudo ~/.install/files/scripts/sudoers.sh
 source ~/.install/files/scripts/utils.sh
 source ~/.install/files/scripts/pyenv.sh
-source ~/.install/files/scripts/ssh.sh
+source ~/.install/files/scripts/ssh-config.sh
 source ~/.install/files/scripts/zsh-omz.sh
 source ~/.install/files/scripts/lightdm.sh
